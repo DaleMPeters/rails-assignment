752eb6b4255b5bd80aa510f9aaa9d6468f14fc60
Dale Peters <dmp9@aber.ac.uk>
2015-11-26 14:23:15 +0000

    changed to use image url instead of id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126114804) do
+ActiveRecord::Schema.define(version: 20151126122520) do
 
   create_table "wines", force: :cascade do |t|
     t.string   "short_description"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20151126114804) do
     t.string   "company"
     t.string   "grape_type"
     t.boolean  "is_vegetarian"
-    t.integer  "picture_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "image_url"
