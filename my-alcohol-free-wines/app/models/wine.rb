d40021a892a9df61b4715fa42def3ea161d492dd
Dale Peters <dmp9@aber.ac.uk>
2015-12-05 13:40:41 +0000

    Changes

--- my-alcohol-free-wines/app/models/wine.rb
+++ my-alcohol-free-wines/app/models/wine.rb
@@ -1,6 +1,5 @@
 class Wine < ActiveRecord::Base
   has_many :basket_items
-  before_destroy :has_no_line_item_references?
 
   def self.search(search)
     if search
@@ -9,15 +8,4 @@ class Wine < ActiveRecord::Base
       @wines = self.all
     end
   end
-
-  private
-    def has_no_line_item_references?
-      if basket_items.empty?
-        return true
-      end
-      errors.add(:base, 'This product is in someone\'s cart, cannot destroy')
-      return false
-    end
-  #end
 end
-
